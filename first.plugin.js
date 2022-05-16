c74388dda60de4d8a8e424958722cda04783fb68
Upsided <matthewlloydw@gmail.com>
2022-05-16 18:35:27 -0500

    chang name

--- first.plugin.js
+++ first.plugin.js
@@ -2,7 +2,7 @@
  * @name FirstPlugin
  * @version 1.0.0
  * @description my first actual plugin lel
- * @author Upsided#0001
+ * @author Upsided
  * @updateUrl https://raw.githubusercontent.com/Upsidedly/bd_firstplugin/master/first.plugin.js
  * @source https://github.com/Upsidedly/bd_firstplugin
  * @authorId 935932557013426176
