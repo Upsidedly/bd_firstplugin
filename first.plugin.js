5a25a5ba13933d0853ee3214bb3aa514a480e26e
Upsided <matthewlloydw@gmail.com>
2022-05-16 18:34:13 -0500

    updated it fr  fr

--- first.plugin.js
+++ first.plugin.js
@@ -1,6 +1,17 @@
 /**
  * @name FirstPlugin
  * @version 1.0.0
- * @description Upsided's first actual plugin lel
+ * @description my first actual plugin lel
  * @author Upsided#0001
+ * @updateUrl https://raw.githubusercontent.com/Upsidedly/bd_firstplugin/master/first.plugin.js
+ * @source https://github.com/Upsidedly/bd_firstplugin
+ * @authorId 935932557013426176
  */
+
+module.exports = class FirstPlugin {
+    load() {
+        BdApi.alert('hi thanks for getting dis')
+    }
+    start() {}
+    stop() {}
+}
